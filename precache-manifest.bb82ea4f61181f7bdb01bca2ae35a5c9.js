bcd1ba6dcf14611ec0be5129f203a2c953df9a26
tractortoby <tobychung1993@gmail.com>
2021-11-10 15:19:04 +0800

    Updates

---
+++ precache-manifest.bb82ea4f61181f7bdb01bca2ae35a5c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66883045dcfd9eff1af289ffb48e04cf",
+    "url": "/react-event-handlers/index.html"
+  },
+  {
+    "revision": "a74c7595e7dccb146f78",
+    "url": "/react-event-handlers/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "6728b8e0f9a3ce0067d5",
+    "url": "/react-event-handlers/static/js/2.e82b4d11.chunk.js"
+  },
+  {
+    "revision": "f0ad72fb0c1c1e4a268423d8869404c9",
+    "url": "/react-event-handlers/static/js/2.e82b4d11.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a74c7595e7dccb146f78",
+    "url": "/react-event-handlers/static/js/main.eeee0ab9.chunk.js"
+  },
+  {
+    "revision": "de1433cd00b5d9402495",
+    "url": "/react-event-handlers/static/js/runtime-main.91cbdc2f.js"
+  }
+]);
